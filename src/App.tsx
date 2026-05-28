c70a19a9965daa57e6dfc47eb64b79480e811151
Gabriel Dantas <gabrieldantas1404@icloud.com>
2026-05-27 23:10:33 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import InputCheckbox from "./components/input-checkbox";
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
+import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
+import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
+import Badge from "./components/badge";
+import Alert from "./components/alert";
+import Divider from "./components/divider";
+import InputText from "./components/input-text";
+import searchIcon from "./assets/icons/search.svg?react"
+import InputSingleFile from "./components/input-single-file";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+	const form = useForm();
+	return (
+		<div className="grid gap-7 p-6">
+			<div className="flex gap-3">
+				<Button>Button</Button>
+				<Button variant="secondary">Button</Button>
+				<Button disabled>Button</Button>
+				<Button handling>Loading</Button>
+				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
+				<Button variant="ghost" size="sm">
+					Button
+				</Button>
+				<Button variant="primary" size="sm">
+					Button
+				</Button>
+			</div>
+
+			<div className="flex gap-3">
+				<ButtonIcon icon={ChevronLeftIcon} />
+				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
+			</div>
+
+			<div className="flex gap-3">
+				<Badge>Todos</Badge>
+				<Badge>Natureza</Badge>
+				<Badge>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+			</div>
+
+			<div>
+				<Alert>
+					Tamanho máximo: 50MB
+					<br />
+					Você pode selecionar arquivos em PNG, JPG, JPEG ou WEBP
+				</Alert>
+			</div>
+
+			<div>
+				<Divider />
+			</div>
+			<InputText icon={searchIcon}></InputText>
+			<InputCheckbox size="md"></InputCheckbox>
+			<InputCheckbox size="sm"></InputCheckbox>
+			<hr />
+			<InputSingleFile form={form} {...form.register("file")}></InputSingleFile>
+
+		</div>
+	);
+}
